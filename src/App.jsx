b4941d99ff69c2a5edd8b9d2c0ba974b5089e11c
Jade <jadequinonez3@gmail.com>
2025-12-09 13:40:15 -0800

    useffect

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Home from "./Home";
 import About from "./About";
 import Education from "./Education";
 import Contact from "./Contact";
+import { useEffect } from "react";
+
 
 function App() {
   useEffect(() => {
