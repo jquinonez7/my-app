eea120b692f0471bbf9755d2c90a29d016fd6bec
Jade <jadequinonez3@gmail.com>
2026-02-23 10:30:41 -0800

    about

--- src/About.jsx
+++ src/About.jsx
@@ -4,20 +4,22 @@ const About = () => {
   return (
     <section id="about" className="about-container">
       <div className="about-content">
-        {/* Left Side - About Me Text */}
         <div className="about-text-container">
           <h2 className="about-title">About Me</h2>
-          <p className="about-text">
-            Hi! I’m <span className="about-highlight">Jade Quinonez</span>. I am
-            <span className="about-highlight"> a student at CSULB studying Computer Science with a concentration in Software Development. </span>
-            <span className="about-highlight"> software engineering.</span> 
-            I use <span className="about-highlight">C++, Java, Python, React, JavaScript, HTML, CSS, and Tailwind CSS.</span>
-          </p>
-          <p className="about-text">
-            Beyond coding, I spend time in the realms of <span className="about-highlight">fitness, cars, and traveling</span>.
-          </p>
-        </div>
 
+          <p className="about-text">
+            Hi! I’m Jade Quinonez. I am a student at CSULB studying Computer Science 
+            with a concentration in Software Development. I am familiar working with Python, Java, HTML, CSS, Javascript,
+            and C++. I have used tools/frameworks such as React, Vue.js, Tailwind CSS, Git, Github, and Linux.
+            Beyond coding, I enjoy spending time with my dog, Butters. She is a 4 year-old Aussie. I also enjoy 
+            fashion, the world of cars, and traveling.
+          </p>
+
+        </div>
+      </div>
+    </section>
+  );
+};
         {/* Right Side - ASCII Art */}
         <div className="ascii-art">
           <pre>
